0a16a7cbac1fcb1b3f74dfd2274924c77252be64
Oneeb Aamer <oneeb47@gmail.com>
2023-08-04 11:18:45 +0100

    finish loss categorical cross entropy implementation

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ X, y = spiral_data(100, 3)
 
 class Layer:
     def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
 
     def forward(self, inputs):
@@ -41,10 +41,19 @@ class Loss:
         data_loss = np.mean(sample_loss)
         return data_loss
 
+
 class Loss_CategoricalCrossEntropy(Loss):
     def forward(self, y_pred, y_true):
         samples = len(y_pred)
-        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        # Handle both one hot encoded or scalar values.
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
 
 
 layer1 = Layer(2, 3)
@@ -58,3 +67,7 @@ activation1.forward(layer1.output)
 layer2.forward(activation1.output)
 activation2.forward(layer2.output)
 print(activation2.output)
+
+loss_function = Loss_CategoricalCrossEntropy()
+loss = loss_function.calc(activation2.output, y)
+print(loss)
